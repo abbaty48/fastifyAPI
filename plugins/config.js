e0f5c1920716be31b8627870ebb06d2c61f87807
Abbatyya <abbaty48@gmail.com>
2025-07-26 13:58:50 +0100

    Implement mongodb datasource plugin

--- plugins/config.js
+++ plugins/config.js
@@ -1,10 +1,11 @@
 const fp = require('fastify-plugin');
 const fe = require('@fastify/env');
- 
+
 module.exports = fp(function(fastify, options, next) {
   fastify.register(fe, {
     confKey: "secrets",
     schema: fastify.getSchema('schema:dotenv')
   });
-  next();<F11>
+  next();
+
 }, {name: 'application-config'});
---
+++ plugins/mongo-datasource.js
@@ -0,0 +1,9 @@
+const fp = require('fastify-plugin')
+const fastifyMongo = require('@fastify/mongodb')
+
+module.exports = fp(async (fastify, opts) => {
+  fastify.register(fastifyMongo, {
+    forceClose: true,
+    url: fastify.secrets.MONGO_URL
+  })
+}, {dependencies: ['application-config']})
